cf12b54e66ccbf5233f9b603c78a200a61c635ac
Tushik <mnsk_5455@mail.ru>
2023-11-04 10:46:01 +0300

    Функции и процедуры. ч2

--- 1.py
+++ 1.py
@@ -1,6 +1,10 @@
 sum_nums = lambda x, y: x+y
+ravn_arr = lambda arr1, arr2: (arr1 + [0] * (len(arr2)-len(arr1))) if len(arr1) < len(arr2) else arr1
 
 arr1 = [1, 3, 5]
-arr2 = [1, 7, 2]
+arr2 = [1, 7, 2, 9]
+
+arr1 = ravn_arr(arr1, arr2)
+arr2 = ravn_arr(arr2, arr1)
 
 print(list(map(sum_nums, arr1, arr2)))
